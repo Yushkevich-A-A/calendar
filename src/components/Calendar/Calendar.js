107c90c4621f47915707e8320e75ddc45435e59b
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-12-08 14:51:06 +0300

    gh-pages

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -5,8 +5,6 @@ import WeekTable from './WeekTable/WeekTable';
 function Calendar(props) {
     let { date } = props;
 
-    date += 2000000000;
-
     return(
         <div className="ui-datepicker">
             <CalendarInformation date={date}/>
